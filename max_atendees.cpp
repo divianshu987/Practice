9f75a689e0bffd6f0190f6f4d28dbcc4fc402443
Divianshu <58361394+divianshu987@users.noreply.github.com>
2021-09-23 15:31:50 +0530

    change in here
    This code is not good enough

--- max_atendees.cpp
+++ max_atendees.cpp
@@ -1,14 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-void code() {
+int32_t main() {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-}
-int32_t main() {
-    code();
     int n;
     cin >> n;
     int start_time, end_time;
